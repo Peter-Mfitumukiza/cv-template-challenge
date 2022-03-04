be400a5403e9ea00aaa3ae874b95ed8f75144673
Mfitumukiza Peter <mfitep6@gmail.com>
2022-03-04 22:07:10 +0200

    styling the page

---
+++ script.js
@@ -0,0 +1,14 @@
+const name = document.querySelector("#name");
+
+function generateRandomColor(){
+    let maxVal = 0xFFFFFF;
+    let randomNumber = Math.random() * maxVal;
+    randomNumber = Math.floor(randomNumber);
+    randomNumber = randomNumber.toString(16);
+    let randColor = randomNumber.padStart(6, 0);
+    return `#${randColor.toUpperCase()}`
+}
+
+const changeColor = () => {
+    document.documentElement.style.setProperty("--blue", `${generateRandomColor()}`)
+}
